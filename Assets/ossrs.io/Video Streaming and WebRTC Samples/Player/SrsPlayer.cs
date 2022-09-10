ff0181cc434d60c0b9a55018268d6058040dc469
winlin <winlin@vip.126.com>
2022-09-10 19:40:36 +0800

    Add files.

---
+++ Assets/ossrs.io/Video Streaming and WebRTC Samples/Player/SrsPlayer.cs
@@ -0,0 +1,172 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.WebRTC;
+
+// See https://docs.unity.cn/Packages/com.unity.webrtc@2.4/manual/tutorial.html
+public class SrsPlayer : MonoBehaviour
+{
+    // The WHIP stream url, to pull WebRTC stream from SRS or other media
+    // servers. Please note that SRS uses `/rtc/v1/whip-play/` as a WebRTC
+    // player, or parameter `action=play` in query string.
+    public string url = "http://localhost:1985/rtc/v1/whip-play/?app=live&stream=livestream";
+    // The RAW image to render the received WebRTC video stream. Generally, it
+    // should be in a Canvas object. Please note that we will scale the image
+    // size according to the video stream resolution.
+    public UnityEngine.UI.RawImage receiveImage;
+    // The audio source to play the received WebRTC audio stream. Please create
+    // a normal audio source.
+    public AudioSource receiveAudio;
+
+    private MediaStream receiveStream;
+    private RTCPeerConnection pc;
+
+    private void Awake()
+    {
+        WebRTC.Initialize();
+        Debug.Log("WebRTC: Initialize ok");
+    }
+
+    private void OnDestroy()
+    {
+        pc?.Close();
+        pc?.Dispose();
+        pc = null;
+
+        WebRTC.Dispose();
+        Debug.Log("WebRTC: Dispose ok");
+    }
+
+    private void Start()
+    {
+        // Start WebRTC update.
+        StartCoroutine(WebRTC.Update());
+
+        // Create object only after WebRTC initialized.
+        pc = new RTCPeerConnection();
+        receiveStream = new MediaStream();
+
+        // Setup player peer connection.
+        pc.OnIceCandidate = candidate =>
+        {
+            Debug.Log($"WebRTC: OnIceCandidate {candidate.ToString()}");
+        };
+        pc.OnIceConnectionChange = state =>
+        {
+            Debug.Log($"WebRTC: OnIceConnectionChange {state.ToString()}");
+        };
+        pc.OnTrack = e =>
+        {
+            receiveStream.AddTrack(e.Track);
+        };
+
+        // Setup player media stream.
+        receiveStream.OnAddTrack = e =>
+        {
+            Debug.Log($"WebRTC: OnAddTrack {e.ToString()}");
+            if (e.Track is VideoStreamTrack videoTrack)
+            {
+                videoTrack.OnVideoReceived += tex =>
+                {
+                    Debug.Log($"WebRTC: OnVideoReceived {videoTrack.ToString()}, tex={tex.width}x{tex.height}");
+                    receiveImage.texture = tex;
+                    receiveImage.rectTransform.sizeDelta = new Vector2(tex.width, tex.height);
+                };
+            }
+            if (e.Track is AudioStreamTrack audioTrack)
+            {
+                Debug.Log($"WebRTC: OnAudioReceived {audioTrack.ToString()}");
+                receiveAudio.SetTrack(audioTrack);
+                receiveAudio.loop = true;
+                receiveAudio.Play();
+            }
+        };
+
+        // Setup PeerConnection to receive stream only.
+        StartCoroutine(SetupPeerConnection());
+        IEnumerator SetupPeerConnection()
+        {
+            RTCRtpTransceiverInit init = new RTCRtpTransceiverInit();
+            init.direction = RTCRtpTransceiverDirection.RecvOnly;
+            pc.AddTransceiver(TrackKind.Audio, init);
+            pc.AddTransceiver(TrackKind.Video, init);
+
+            yield return StartCoroutine(PeerNegotiationNeeded());
+        }
+
+        // Generate offer.
+        IEnumerator PeerNegotiationNeeded()
+        {
+            var op = pc.CreateOffer();
+            yield return op;
+
+            Debug.Log($"WebRTC: CreateOffer done={op.IsDone}, hasError={op.IsError}, {op.Desc}");
+            if (op.IsError) yield break;
+
+            yield return StartCoroutine(OnCreateOfferSuccess(op.Desc));
+        }
+
+        // When offer is ready, set to local description.
+        IEnumerator OnCreateOfferSuccess(RTCSessionDescription offer)
+        {
+            var op = pc.SetLocalDescription(ref offer);
+            Debug.Log($"WebRTC: SetLocalDescription {offer.type} {offer.sdp}");
+            yield return op;
+
+            Debug.Log($"WebRTC: Offer done={op.IsDone}, hasError={op.IsError}");
+            if (op.IsError) yield break;
+
+            yield return StartCoroutine(ExchangeSDP(url, offer.sdp));
+        }
+
+        // Exchange SDP(offer) with server, got answer.
+        IEnumerator ExchangeSDP(string url, string offer)
+        {
+            // Use Task to call async methods.
+            var task = System.Threading.Tasks.Task<string>.Run(async () =>
+            {
+                System.Uri uri = new System.UriBuilder(url).Uri;
+                Debug.Log($"WebRTC: Build uri {uri}");
+
+                var content = new System.Net.Http.StringContent(offer);
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/sdp");
+
+                var client = new System.Net.Http.HttpClient();
+                var res = await client.PostAsync(uri, content);
+                res.EnsureSuccessStatusCode();
+
+                string data = await res.Content.ReadAsStringAsync();
+                Debug.Log($"WebRTC: Exchange SDP ok, answer is {data}");
+                return data;
+            });
+
+            // Covert async to coroutine yield, wait for task to be completed.
+            yield return new WaitUntil(() => task.IsCompleted);
+            // Check async task exception, it won't throw it automatically.
+            if (task.Exception != null)
+            {
+                Debug.Log($"WebRTC: Exchange SDP failed, url={url}, err is {task.Exception.ToString()}");
+                yield break;
+            }
+
+            StartCoroutine(OnGotAnswerSuccess(task.Result));
+        }
+
+        // When got answer, set remote description.
+        IEnumerator OnGotAnswerSuccess(string answer)
+        {
+            RTCSessionDescription desc = new RTCSessionDescription();
+            desc.type = RTCSdpType.Answer;
+            desc.sdp = answer;
+            var op = pc.SetRemoteDescription(ref desc);
+            yield return op;
+
+            Debug.Log($"WebRTC: Answer done={op.IsDone}, hasError={op.IsError}");
+            yield break;
+        }
+    }
+
+    private void Update()
+    {
+    }
+}
---
+++ Assets/ossrs.io/Video Streaming and WebRTC Samples/Publisher/SrsPublisher.cs
@@ -0,0 +1,226 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.WebRTC;
+
+public class SrsPublisher : MonoBehaviour
+{
+    // The WHIP stream url, to push WebRTC stream to SRS or other media servers.
+    public string url = "http://localhost:1985/rtc/v1/whip/?app=live&stream=livestream";
+    // A RAW image to render the camera stream, a preview of camera. Please note
+    // that we will randomly choose a camera.
+    public UnityEngine.UI.RawImage sourceImage;
+    // A audio source to capture the audio of microphone. Please note that we
+    // will randomly choose a microphone.
+    public AudioSource sourceAudio;
+
+    private WebCamTexture webCamTexture;
+    private VideoStreamTrack videoStreamTrack;
+    private AudioStreamTrack audioStreamTrack;
+    private RTCPeerConnection pc;
+
+    private void Awake()
+    {
+        WebRTC.Initialize();
+        Debug.Log("WebRTC: Initialize ok");
+    }
+
+    private void OnDestroy()
+    {
+        videoStreamTrack?.Dispose();
+        videoStreamTrack = null;
+
+        audioStreamTrack?.Dispose();
+        audioStreamTrack = null;
+
+        pc?.Close();
+        pc?.Dispose();
+        pc = null;
+
+        webCamTexture?.Stop();
+        webCamTexture = null;
+
+        WebRTC.Dispose();
+        Debug.Log("WebRTC: Dispose ok");
+    }
+
+    void Start()
+    {
+        // Start WebRTC update.
+        StartCoroutine(WebRTC.Update());
+
+        // Create object only after WebRTC initialized.
+        pc = new RTCPeerConnection();
+
+        // Setup player peer connection.
+        pc.OnIceCandidate = candidate =>
+        {
+            Debug.Log($"WebRTC: OnIceCandidate {candidate.ToString()}");
+        };
+        pc.OnIceConnectionChange = state =>
+        {
+            Debug.Log($"WebRTC: OnIceConnectionChange {state.ToString()}");
+        };
+        pc.OnTrack = e =>
+        {
+            Debug.Log($"WebRTC: OnTrack {e.Track.Kind} id={e.Track.Id}");
+        };
+
+        // Setup PeerConnection to send stream only.
+        StartCoroutine(SetupPeerConnection());
+        IEnumerator SetupPeerConnection()
+        {
+            RTCRtpTransceiverInit init = new RTCRtpTransceiverInit();
+            init.direction = RTCRtpTransceiverDirection.SendOnly;
+            pc.AddTransceiver(TrackKind.Audio, init);
+            pc.AddTransceiver(TrackKind.Video, init);
+
+            yield return StartCoroutine(StartCamera());
+        }
+
+        // Start cemara device.
+        IEnumerator StartCamera()
+        {
+            // Request user authorization.
+            yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+            if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
+            {
+                Debug.LogFormat($"WebRTC: Authorization for using the camera is denied");
+                yield break;
+            }
+
+            // Select camera device.
+            WebCamDevice device = new WebCamDevice();
+            foreach (var cam in WebCamTexture.devices)
+            {
+                if (device.name == null || cam.name.ToLower().Contains("built-in")) device = cam;
+                Debug.Log($"WebRTC: Camera device {cam.name} kind={cam.kind}, front={cam.isFrontFacing}");
+            }
+            Debug.Log($"WebRTC: Camera {device.name} selected");
+
+            webCamTexture = new WebCamTexture(device.name);
+            webCamTexture.Play();
+
+            yield return new WaitUntil(() => webCamTexture.didUpdateThisFrame);
+            Debug.Log($"WebRTC: Camera {device.name} kind={device.kind} is open, texture={webCamTexture.width}x{webCamTexture.height}");
+
+            videoStreamTrack = new VideoStreamTrack(webCamTexture);
+            sourceImage.texture = webCamTexture;
+            sourceImage.rectTransform.sizeDelta = new Vector2(webCamTexture.width, webCamTexture.height);
+            pc.AddTrack(videoStreamTrack);
+            Debug.Log($"WebRTC: Add video track {videoStreamTrack.Id}");
+
+            yield return StartCoroutine(StartMicrophone());
+        }
+
+        // Start microphone device.
+        IEnumerator StartMicrophone()
+        {
+            // Request user authorization.
+            yield return Application.RequestUserAuthorization(UserAuthorization.Microphone);
+            if (!Application.HasUserAuthorization(UserAuthorization.Microphone))
+            {
+                Debug.LogFormat($"WebRTC: Authorization for using the microphone is denied");
+                yield break;
+            }
+
+            string device = null;
+            foreach (var mic in Microphone.devices)
+            {
+                if (device == null || mic.ToLower().Contains("microphone")) device = mic;
+                Debug.Log($"WebRTC: Microphone device {mic}");
+            }
+            Debug.Log($"WebRTC: Microphone {device} selected");
+
+            Microphone.GetDeviceCaps(device, out int minFreq, out int maxFreq);
+            var clip = Microphone.Start(device, true, 1, 48000);
+
+            // set the latency to “0” samples before the audio starts to play.
+            while (!(Microphone.GetPosition(device) > 0)) { }
+
+            sourceAudio.clip = clip;
+            sourceAudio.loop = true;
+            sourceAudio.Play();
+            audioStreamTrack = new AudioStreamTrack(sourceAudio);
+            pc.AddTrack(audioStreamTrack);
+            Debug.Log($"WebRTC: Add audio track {audioStreamTrack.Id}");
+
+            yield return StartCoroutine(PeerNegotiationNeeded()); ;
+        }
+
+        // Generate offer.
+        IEnumerator PeerNegotiationNeeded()
+        {
+            var op = pc.CreateOffer();
+            yield return op;
+
+            Debug.Log($"WebRTC: CreateOffer done={op.IsDone}, hasError={op.IsError}, {op.Desc}");
+            if (op.IsError) yield break;
+
+            yield return StartCoroutine(OnCreateOfferSuccess(op.Desc));
+        }
+
+        // When offer is ready, set to local description.
+        IEnumerator OnCreateOfferSuccess(RTCSessionDescription offer)
+        {
+            var op = pc.SetLocalDescription(ref offer);
+            Debug.Log($"WebRTC: SetLocalDescription {offer.type} {offer.sdp}");
+            yield return op;
+
+            Debug.Log($"WebRTC: Offer done={op.IsDone}, hasError={op.IsError}");
+            if (op.IsError) yield break;
+
+            yield return StartCoroutine(ExchangeSDP(url, offer.sdp));
+        }
+
+        // Exchange SDP(offer) with server, got answer.
+        IEnumerator ExchangeSDP(string url, string offer)
+        {
+            // Use Task to call async methods.
+            var task = System.Threading.Tasks.Task<string>.Run(async () =>
+            {
+                System.Uri uri = new System.UriBuilder(url).Uri;
+                Debug.Log($"WebRTC: Build uri {uri}");
+
+                var content = new System.Net.Http.StringContent(offer);
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/sdp");
+
+                var client = new System.Net.Http.HttpClient();
+                var res = await client.PostAsync(uri, content);
+                res.EnsureSuccessStatusCode();
+
+                string data = await res.Content.ReadAsStringAsync();
+                Debug.Log($"WebRTC: Exchange SDP ok, answer is {data}");
+                return data;
+            });
+
+            // Covert async to coroutine yield, wait for task to be completed.
+            yield return new WaitUntil(() => task.IsCompleted);
+            // Check async task exception, it won't throw it automatically.
+            if (task.Exception != null)
+            {
+                Debug.Log($"WebRTC: Exchange SDP failed, url={url}, err is {task.Exception.ToString()}");
+                yield break;
+            }
+
+            StartCoroutine(OnGotAnswerSuccess(task.Result));
+        }
+
+        // When got answer, set remote description.
+        IEnumerator OnGotAnswerSuccess(string answer)
+        {
+            RTCSessionDescription desc = new RTCSessionDescription();
+            desc.type = RTCSdpType.Answer;
+            desc.sdp = answer;
+            var op = pc.SetRemoteDescription(ref desc);
+            yield return op;
+
+            Debug.Log($"WebRTC: Answer done={op.IsDone}, hasError={op.IsError}");
+            yield break;
+        }
+    }
+
+    void Update()
+    {
+    }
+}
